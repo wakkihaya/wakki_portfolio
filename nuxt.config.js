c738d9168b14ec184eccc8e2fefabefae478a7b9
wakkihaya <wakkihaya@gmail.com>
2019-07-08 00:42:14 +0900

    初めの画面

---
+++ nuxt.config.js
@@ -0,0 +1,42 @@
+module.exports = {
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: 'wakki_portfolio',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'this displays all of things in wakki&apos;s life' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+ css: [
+   '~/assets/sass/main.scss'
+ ],
+  /*
+  ** Customize the progress bar color
+  */
+  loading: { color: '#3B8070' },
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** Run ESLint on save
+    */
+    extend (config, { isDev, isClient }) {
+      if (isDev && isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    }
+  }
+}
+
---
+++ pages/index.vue
@@ -0,0 +1,92 @@
+<template>
+  <section class="container">
+<!--   headタグ-->
+    <div id="first-page">
+      <div class="bg-mask">
+
+<!--        中身-->
+        <div id="head">
+     <p>Hayato Waki</p>
+   </div>
+
+    <div id="title">
+      <p>
+      Entrepreneur and <br>
+        Web engineer <br>
+      who join some projects and hold some events.
+      </p>
+    </div>
+
+<!--        menu-box-->
+        <ul id="menu-box">
+          <li>Introduction</li>
+          <li>Activity</li>
+          <li>Products</li>
+          <li>Contact</li>
+        </ul>
+
+      </div>
+
+      <div id="introduction">
+
+      </div>
+
+
+    </div>
+
+
+
+  </section>
+</template>
+
+<script>
+import '~/assets/sass/main.scss'
+
+  export default {
+    name: ''
+  };
+
+</script>
+
+<!--<style lang="scss">-->
+<!--.container {-->
+<!--  min-height: 100vh;-->
+<!--  display: flex;-->
+<!--  /*justify-content: center;*/-->
+<!--  /*align-items: center;*/-->
+<!--  /*text-align: center;*/-->
+<!--  flex-direction:column;-->
+<!--}-->
+
+<!--#head{-->
+<!--  position:fixed;-->
+<!--  font-family: "Century";-->
+<!--  width:30vw;-->
+<!--  p{-->
+<!--    font-size:10px;-->
+<!--  }-->
+<!--}-->
+
+
+<!--.title {-->
+<!--  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */-->
+<!--  display: block;-->
+<!--  font-weight: 300;-->
+<!--  font-size: 100px;-->
+<!--  color: #35495e;-->
+<!--  letter-spacing: 1px;-->
+<!--}-->
+
+<!--.subtitle {-->
+<!--  font-weight: 300;-->
+<!--  font-size: 42px;-->
+<!--  color: #526488;-->
+<!--  word-spacing: 5px;-->
+<!--  padding-bottom: 15px;-->
+<!--}-->
+
+<!--.links {-->
+<!--  padding-top: 15px;-->
+<!--&lt;!&ndash;}&ndash;&gt;-->
+<!--</style>-->
+
