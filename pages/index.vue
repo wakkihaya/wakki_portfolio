f51a5f49763ba2dc8746c385c9a0a9b41e08f5e4
wakkihaya <wakkihaya@gmail.com>
2019-08-02 20:33:06 +0900

    nuxtの調整

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,10 @@
 <template>
 
   <section class="container">
-<!--   headタグ-->
+    <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
+
+    <!--   headタグ-->
+
     <div id="first-page">
       <div class="bg-mask">
 
@@ -51,15 +54,14 @@
       {{ message }}
     </p>
 
-<!--        <table border="0" v-for = "career in careers" v-bind:key="career.year" id="mycareer">-->
-<!--          <tbody>-->
-<!--          <tr>-->
-<!--            <td class="td1">a</td>-->
-<!--&lt;!&ndash;            <td class="td1">{{career.year}}</td>&ndash;&gt;-->
-<!--            <td class="td2" style="white-space:pre-line">{{career.event }}</td>-->
-<!--          </tr>-->
-<!--          </tbody>-->
-<!--        </table>-->
+        <table border="0" v-for = "career in careers" v-bind:key="career.year" id="mycareer">
+          <tbody>
+          <tr>
+            <td class="td1">{{career.year}}</td>
+            <td class="td2" style="white-space:pre-line">{{career.event }}</td>
+          </tr>
+          </tbody>
+        </table>
 
       </div>
 
@@ -83,57 +85,37 @@
   </section>
 </template>
 
-<link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
-
 <script>
 import '~/assets/sass/main.scss';
 import Vue from 'vue';
 
+
 export default {
-  name: "introduction",
-  data(){
-    return{
-      message:'unti'
-    }
+  name: 'mycareer',
+  data() {
+    return {
+      careers: [
+        {year: '1999', event: 'Born in Japan'},
+        {year: '2014', event: 'Entered Seijo high school'},
+        {year: '2017', event: 'Entered Computer Science in Univ of Tsukuba'},
+        {
+          year: '2018',
+          event: '・ Interned as an engineer in USEN-NEXT-GROUP Inc\n\n' +
+            '・ Interned as a web engineer in Unipro Inc\n\n' +
+            '・ Started the volunteer activity with foreign students as a chief'
+        },
+        {year: 'now', event: 'Start up curation media business'},
+        {
+          year: 'future',
+          event: '・Continue the media business\n\n' +
+            '・Join in the business contest of Microsoft in Japan\n\n' +
+            '・Study CS in SF State Univ and work in San Francisco'
+        }
+      ],
+      message:"unti"
+    };
   }
-}
-
-
-//   name: 'career_div',
-//   data: function () {
-//     return{
-//       message: 'unti',
-//     }
-//   }
-// }
-
-
-// export default {
-//   name: 'mycareer',
-//   data() {
-//     return {
-//       careers: [
-//         {year: '1999', event: 'Born in Japan'},
-//         {year: '2014', event: 'Entered Seijo high school'},
-//         {year: '2017', event: 'Entered Computer Science in Univ of Tsukuba'},
-//         {
-//           year: '2018',
-//           event: '・ Interned as an engineer in USEN-NEXT-GROUP Inc\n\n' +
-//             '・ Interned as a web engineer in Unipro Inc\n\n' +
-//             '・ Started the volunteer activity with foreign students as a chief'
-//         },
-//         {year: 'now', event: 'Start up curation media business'},
-//         {
-//           year: 'future',
-//           event: '・Continue the media business\n\n' +
-//             '・Join in the business contest of Microsoft in Japan\n\n' +
-//             '・Study CS in SF State Univ and work in San Francisco'
-//         }
-//       ],
-//       message:"unti"
-//     };
-//   }
-// };
+};
 
 </script>
 
