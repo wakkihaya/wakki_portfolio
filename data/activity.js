3f1e1aadd1e87ad8f0a3b323aae5cd2c1d587b6a
wakkihaya <wakkihaya@gmail.com>
2021-06-10 10:58:18 +0900

    fix: space between texts

--- data/activity.js
+++ data/activity.js
@@ -7,10 +7,10 @@ const activityList = [
     content: 'Feedal has clear vision that "anyone can get whatever kinds of information immediately with no stress." ' +
       "And we're trying to create services in terms of news, communication tools, etc.",
     achievement:
-      '・Direct software design and specs, including web, mobile, and desktop- native applications.\n' +
+      '・Direct software design and specs, including web, mobile, and desktop-native applications.\n' +
       '・Define specs with user feedback, and execute prototyping design, and develop applications.\n' +
-      '・Acquire 1.5K MAU for the COVID-19 app and 30 early-adaptors / teams for multiple apps with business strategies. \n' +
-      '・Host the engineering community in the university with 50 + developers.',
+      '・Acquire 1.5K MAU for the COVID-19 app and 30 early-adaptors/teams for multiple apps with business strategies. \n' +
+      '・Host the engineering community in the university with 50+ developers.',
     link: 'https://corp.feedal.com/',
     image: require('~/assets/images/feedal.jpg')
   },
@@ -41,8 +41,8 @@ const activityList = [
     position: "CTO/Co-Founder",
     content: "'Here' is the drop-in voice call app for long-distance couples, families and BFFs. We're in private beta.",
     achievement:
-      '・Develop and execute all development systems such as infrastructure, database architecture, front/ back-end, and analytics.\n' +
-      '・Analyze data and user behaviors, and get 20 % more app - open counts per retaining user by implementing features.\n' +
+      '・Develop and execute all development systems such as infrastructure, database architecture, front/back-end, and analytics.\n' +
+      '・Analyze data and user behaviors, and get 20 % more app-open counts per retaining user by implementing features.\n' +
       '・Success with fund-raising $300K for pre-seed, and also with getting 100 supporters by crowd-funding.',
     image: require("~/assets/images/here-logo.png")
   },
