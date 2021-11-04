f114db8a16ffb3d1b8cc8f41414726e320501f22
wakkihaya <wakkihaya@gmail.com>
2021-11-04 16:07:11 +0900

    update: career

--- data/career.js
+++ data/career.js
@@ -1,90 +1,103 @@
 const careerList = [
   {
-    year: '2018',
-    event: 'Business internship\n\nat USEN-NEXT-GROUP,Inc',
-    location_and_date: 'Tokyo/Japan, Aug 2018',
-    atag: 'https://usen-next.co.jp/index.html',
+    year: "2018",
+    event: "Business internship\n\nat USEN-NEXT-GROUP,Inc",
+    location_and_date: "Tokyo/Japan, Aug 2018",
+    atag: "https://usen-next.co.jp/index.html",
     content:
-      '・Research on potential needs in the agriculture industry and build a new business idea in a team.\n\n' +
-      '・Pitch our idea.'
+      "・Research on potential needs in the agriculture industry and build a new business idea in a team.\n\n" +
+      "・Pitch our idea.",
   },
   {
-    year: '2018',
-    event: 'Office system engineer internship\n\nat USEN-ICT-SOLUTIONS,Inc',
-    location_and_date: 'Tokyo/Japan, Dec 2018 - Feb 2019',
-    atag: 'https://usen-ict.co.jp/',
+    year: "2018",
+    event: "Office system engineer internship\n\nat USEN-ICT-SOLUTIONS,Inc",
+    location_and_date: "Tokyo/Japan, Dec 2018 - Feb 2019",
+    atag: "https://usen-ict.co.jp/",
     content:
-      '・Build an automated mail system connected with Google spreadsheet by using GAS.\n\n' +
-      '・Build automated marketing analytics tool for finding potential customers.'
+      "・Build an automated mail system connected with Google spreadsheet by using GAS.\n\n" +
+      "・Build automated marketing analytics tool for finding potential customers.",
   },
   {
-    year: '2019',
-    event: 'Full stack engineer internship\n\nat Unipro,Inc',
-    location_and_date: 'Tokyo/Japan, Feb 2019 - Apr 2019',
+    year: "2019",
+    event: "Full stack engineer internship\n\nat Unipro,Inc",
+    location_and_date: "Tokyo/Japan, Feb 2019 - Apr 2019",
     content:
-      '・Develop web applications agilely using Vue.js and Laravel in a team.\n\n' +
-      '・Execute as a test engineer along with app requirements.'
+      "・Develop web applications agilely using Vue.js and Laravel in a team.\n\n" +
+      "・Execute as a test engineer along with app requirements.",
   },
   {
-    year: '2019',
-    event: 'Business internship\n\nat Microsoft Japan',
-    atag: 'https://www.microsoft.com/ja-jp',
-    location_and_date: 'Tokyo/Japan, Aug 2019',
+    year: "2019",
+    event: "Business internship\n\nat Microsoft Japan",
+    atag: "https://www.microsoft.com/ja-jp",
+    location_and_date: "Tokyo/Japan, Aug 2019",
     content:
-      '・Research on technologies and communities which Microsoft has.\n\n' +
-      '・Create a business idea(Education service) from the perspective of the company vision and monetization.\n\n' +
-      '・Execute prototyping design of the service by Figma.'
+      "・Research on technologies and communities which Microsoft has.\n\n" +
+      "・Create a business idea(Education service) from the perspective of the company vision and monetization.\n\n" +
+      "・Execute prototyping design of the service by Figma.",
   },
   {
-    year: '2019',
-    event: 'Developer internship\n\nat Btrax, Inc',
-    atag: 'https://btrax.com/',
-    location_and_date: 'San Francisco/U.S., Oct 2019 - Dec 2019',
+    year: "2019",
+    event: "Developer internship\n\nat Btrax, Inc",
+    atag: "https://btrax.com/",
+    location_and_date: "San Francisco/U.S., Oct 2019 - Dec 2019",
     content:
-      '・Build deploy environment for DevOps by connecting with Github and AWS.\n\n' +
-      '・Develop the websites using PHP with backend system by Zapier.\n\n' +
-      '・Create original tools for office system improvement by using Node.js with APIs.'
+      "・Build deploy environment for DevOps by connecting with Github and AWS.\n\n" +
+      "・Develop the websites using PHP with backend system by Zapier.\n\n" +
+      "・Create original tools for office system improvement by using Node.js with APIs.",
   },
   {
-    year: '2020',
-    event: 'CEO/Co-Founder\n\nat Feedal,Inc',
-    atag: 'https://corp.feedal.com',
-    location_and_date: 'Tsukuba/Japan, Jul 2019 - Oct 2020',
-    content: '・Direct software design and specs, including web, mobile, and desktop-native applications.\n\n' +
-      '・Define specs with user feedback, and execute prototyping design, and develop applications.\n\n' +
-      '・Acquire 1.5K MAU for the COVID-19 app and 30 early-adaptors / teams for multiple apps with business strategies.\n\n' +
-      '・Host the engineering community in the university with 50 + developers.',
-  },
-  {
-    year: '2020',
-    event: 'Software Engineer Internship\n\nat Locotabi,Inc',
-    atga: 'https://locotabi.jp',
-    location_and_date: 'Tokyo/Japan, Sep 2020 - Oct 2020',
+    year: "2020",
+    event: "CEO/Co-Founder\n\nat Feedal,Inc",
+    atag: "https://corp.feedal.com",
+    location_and_date: "Tsukuba/Japan, Jul 2019 - Oct 2020",
     content:
-      '・Improve features and fix bugs in app using Ruby on Rails',
+      "・Direct software design and specs, including web, mobile, and desktop-native applications.\n\n" +
+      "・Define specs with user feedback, and execute prototyping design, and develop applications.\n\n" +
+      "・Acquire 1.5K MAU for the COVID-19 app and 30 early-adaptors / teams for multiple apps with business strategies.\n\n" +
+      "・Host the engineering community in the university with 50 + developers.",
   },
   {
-    year: '2021',
-    event: 'CTO/Co-Founder\n\nat Here labs, Inc',
-    location_and_date: 'San Francisco/U.S., Dec 2020 - Apr 2021',
-    content: '・Develop and execute all development systems such as infrastructure, database architecture, front/ back-end, and analytics.\n\n' +
-      '・Analyze data and user behaviors, and get 20 % more app-open counts per retaining user by implementing features.\n\n' +
-      '・Success with fund-raising $300K for pre-seed, and also with getting 100 supporters by crowd-funding.'
+    year: "2020",
+    event: "Software Engineer Internship\n\nat Locotabi,Inc",
+    atga: "https://locotabi.jp",
+    location_and_date: "Tokyo/Japan, Sep 2020 - Oct 2020",
+    content: "・Improve features and fix bugs in app using Ruby on Rails",
   },
   {
-    year: '2021',
-    event: 'Writer/Orgnizer at\n\nNeurotechJP blogsite',
-    atag: 'https://neurotechjp.com',
-    location_and_date: 'Remote, May 2021 - Present',
-    content: '',
+    year: "2021",
+    event: "CTO/Co-Founder\n\nat Here labs, Inc",
+    location_and_date: "San Francisco/U.S., Dec 2020 - Apr 2021",
+    content:
+      "・Develop and execute all development systems such as infrastructure, database architecture, front/ back-end, and analytics.\n\n" +
+      "・Analyze data and user behaviors, and get 20 % more app-open counts per retaining user by implementing features.\n\n" +
+      "・Success with fund-raising $300K for pre-seed, and also with getting 100 supporters by crowd-funding.",
   },
   {
-    year: '2021',
-    event: 'PdM (Contractor)\n\nat Datafluct',
-    atag: 'https://datafluct.com',
-    location_and_date: 'Tokyo/Japan, July 2021 - Present',
-    content: '',
-  }
+    year: "2021",
+    event: "Writer/Orgnizer at\n\nNeurotechJP blogsite",
+    atag: "https://neurotechjp.com",
+    location_and_date: "Remote, May 2021 - Present",
+    content:
+      "・Interview founders and researchers who stand in the front line in neurotechnology space.\n\n" +
+      "・Write blog posts of the interviews and columns.\n\n" +
+      "・Research the market in this space.",
+  },
+  {
+    year: "2021",
+    event: "PdM (Contractor)\n\nat Datafluct",
+    atag: "https://datafluct.com",
+    location_and_date: "Tokyo/Japan, July 2021 - Present",
+    content:
+      "・Create specifications of multiple products in the AI agent project.\n\n" +
+      "・Operate development for apps and algorithms.",
+  },
+  {
+    year: "2021",
+    event: "Product Engineer \n\nat Neurons Inc",
+    atag: "https://neuronsinc.com",
+    location_and_date: "Denmark, November 2021 - Present",
+    content: "",
+  },
 ]
 
 export default careerList
