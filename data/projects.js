f07e465210b6261dcdbb228d8e62e59b1b641ed9
wakkihaya <wakkihaya@gmail.com>
2022-02-15 16:44:24 +0900

    fix: research

--- data/projects.js
+++ data/projects.js
@@ -1,48 +1,56 @@
 const projectsList = [
   {
     title: '[OnGoing!]\n\n"NeurotechJP" (Media)',
-    content: "NeurotechJP is a media website for neurotechnology.",
-    image: require('~/assets/images/recent-works/neurotech/neurotechJp.png'),
-    atag: 'https://neurotechjp.com'
+    content: "NeurotechJP is a neurotech interview media.",
+    image: require("~/assets/images/recent-works/neurotech/neurotechJp.png"),
+    atag: "https://neurotechjp.com",
   },
   {
-    title: '[OnGoing!]\n\nBCI research project (University)',
-    content: "A individual research project of SSVEP-based BCI in the univeristy.",
+    title: "BCI research project (University)",
+    content:
+      "An individual research project of Emotional BCI in the univeristy. Clarified relationship between P300 and emotions.",
   },
   {
     title: '"Here" (Startup)',
-    content: "We(Here Labs,Inc) launched 'Here', a voice/video communication app for couples.",
-    image: require('~/assets/images/recent-works/projects/Here.jpg')
+    content:
+      "We(Here Labs,Inc) launched 'Here', a voice/video communication app for couples.",
+    image: require("~/assets/images/recent-works/projects/Here.jpg"),
   },
   {
     title: '"Funbrella" (University)',
-    content: "A team project at Univ of Tsukuba. We developed `Funbrella`,the umbrella that projects a screen of your smartphone, and also launched the game using with 'Funbrella'",
-    image: require('~/assets/images/recent-works/projects/funbrella.png')
+    content:
+      "A team project at Univ of Tsukuba. We developed `Funbrella`,the umbrella that projects a screen of your smartphone, and also launched the game using with 'Funbrella'",
+    image: require("~/assets/images/recent-works/projects/funbrella.png"),
   },
   {
     title: '"GatorTrader" (University)',
-    content: "A team project at SFSU. We developed 'GatorTrader',C2C EC site for SFSU students.",
-    image: require('~/assets/images/recent-works/projects/gatortrader_formal.png')
+    content:
+      "A team project at SFSU. We developed 'GatorTrader',C2C EC site for SFSU students.",
+    image: require("~/assets/images/recent-works/projects/gatortrader_formal.png"),
   },
   {
     title: '"paroli" (Startup)',
-    content: "We(Feedal,Inc) launched 'paroli', a remote-work communication tool with voice messages and real-time voice meetings.",
-    image: require('~/assets/images/recent-works/projects/paroli.jpg')
+    content:
+      "We(Feedal,Inc) launched 'paroli', a remote-work communication tool with voice messages and real-time voice meetings.",
+    image: require("~/assets/images/recent-works/projects/paroli.jpg"),
   },
   {
     title: '"vs.corona" (Startup)',
-    content: "We(Feedal,Inc) launched 'vs.corona', a curation website gathering COVID-19 news from reliable resouces such as governments.",
-    image: require('~/assets/images/recent-works/projects/vs-corona.png')
+    content:
+      "We(Feedal,Inc) launched 'vs.corona', a curation website gathering COVID-19 news from reliable resouces such as governments.",
+    image: require("~/assets/images/recent-works/projects/vs-corona.png"),
   },
   {
     title: '"Feedal" (Startup)',
-    content: "We(Feedal,Inc) launched 'Feedal', a news-sharing platform for engineers.",
-    image: require('~/assets/images/recent-works/projects/feedal.jpg')
+    content:
+      "We(Feedal,Inc) launched 'Feedal', a news-sharing platform for engineers.",
+    image: require("~/assets/images/recent-works/projects/feedal.jpg"),
   },
   {
-    title: 'Microsoft Business contest (Internship)',
-    content: "Our team got 3rd place with a educative product 'Microsoft Canvas'.",
-    image: require('~/assets/images/recent-works/projects/MS_business_contest.png')
+    title: "Microsoft Business contest (Internship)",
+    content:
+      "Our team got 3rd place with a educative product 'Microsoft Canvas'.",
+    image: require("~/assets/images/recent-works/projects/MS_business_contest.png"),
   },
 ]
 
