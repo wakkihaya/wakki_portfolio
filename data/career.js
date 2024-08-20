fbaafa6a309b46400a98c4611f60fd8f63e70aab
wakkihaya <wakkihaya@gmail.com>
2024-08-20 16:50:05 +0900

    chore: add career

--- data/career.js
+++ data/career.js
@@ -5,8 +5,7 @@ const careerList = [
     location_and_date: "Tokyo/Japan, Aug 2018",
     atag: "https://usen-next.co.jp/index.html",
     content:
-      "・Research on potential needs in the agriculture industry and build a new business idea in a team.\n\n" +
-      "・Pitch our idea.",
+      "5-Day Business Development Internship.\n\n - Conducted research on potential needs in the agriculture industry and collaborated with a team to develop a new business concept.\n - Presented and pitched our business idea to stakeholders.",
   },
   {
     year: "2018",
@@ -14,16 +13,14 @@ const careerList = [
     location_and_date: "Tokyo/Japan, Dec 2018 - Feb 2019",
     atag: "https://usen-ict.co.jp/",
     content:
-      "・Build an automated mail system connected with Google spreadsheet by using GAS.\n\n" +
-      "・Build an automated marketing analytics tool for finding potential customers.",
+      "The company managed a large volume of client data and emails through G Suite, leading to significant redundancies.\n\n - Developed an automated email system integrated with Google Sheets using Google Apps Script (GAS).\n - Created an automated marketing analytics tool to identify potential customers.",
   },
   {
     year: "2019",
     event: "Full stack engineer internship\n\nat Unipro,Inc",
     location_and_date: "Tokyo/Japan, Feb 2019 - Apr 2019",
     content:
-      "・Develop web applications agilely using Vue.js and Laravel in a team.\n\n" +
-      "・Execute as a test engineer along with app requirements.",
+      "Unipro is a software agency specializing in web application development.\n\n - Collaborated in an agile team to develop web applications using Vue.js and Laravel.\n - Served as a test engineer, ensuring applications met all requirements.",
   },
   {
     year: "2019",
@@ -31,9 +28,7 @@ const careerList = [
     atag: "https://www.microsoft.com/ja-jp",
     location_and_date: "Tokyo/Japan, Aug 2019",
     content:
-      "・Research on technologies and communities which Microsoft has.\n\n" +
-      "・Create a business idea(Education service) from the perspective of the company vision and monetization.\n\n" +
-      "・Execute prototyping design of the service by Figma.",
+      "The internship was a business contest hosted by Microsoft Japan, where teams developed new business ideas in a week.\n\n - Researched Microsoft's technologies and communities.\n - Created a business idea for an education service aligned with the company's vision and monetization strategies.\n - Executed the service's prototype design using Figma. Our team secured 3rd place out of 10 teams",
   },
   {
     year: "2019",
@@ -41,9 +36,7 @@ const careerList = [
     atag: "https://btrax.com/",
     location_and_date: "San Francisco/U.S., Oct 2019 - Dec 2019",
     content:
-      "・Build the deployment environment for DevOps by connecting with Github and AWS.\n\n" +
-      "・Develop the websites using PHP with backend system by Zapier.\n\n" +
-      "・Create original tools for office system improvement by using Node.js with APIs.",
+      "Btrax is a design consulting company that uses design thinking methodology to identify potential problems and build mock-ups with clients.\n\n - Built and deployed a DevOps environment by integrating GitHub and AWS.\n - Developed websites using PHP, with backend systems connected via Zapier.\n - Created custom tools to improve office systems using Node.js and various APIs.",
   },
   {
     year: "2020",
@@ -51,35 +44,22 @@ const careerList = [
     atag: "https://corp.feedal.com",
     location_and_date: "Tsukuba/Japan, Jul 2019 - Oct 2020",
     content:
-      "・Direct software design and specs, including web, mobile, and desktop-native applications.\n\n" +
-      "・Define specs with user feedback, and execute prototyping design, and develop applications.\n\n" +
-      "・Acquire 1.5K MAU for the COVID-19 app and 30 early-adaptors / teams for multiple apps with business strategies.\n\n" +
-      "・Host the engineering community in the university with 50 + developers.",
-  },
-  {
-    year: "2020",
-    event: "Software Engineer Internship\n\nat Locotabi,Inc",
-    atga: "https://locotabi.jp",
-    location_and_date: "Tokyo/Japan, Sep 2020 - Oct 2020",
-    content: "・Improve features and fix bugs in the app using Ruby on Rails",
+      "My three university friends and I founded a company dedicated to software applications, where we developed and scaled multiple apps with stable revenue.\n\n - Directed software design and specifications for web, mobile, and desktop-native applications.\n - Defined specs based on user feedback, executed prototype designs, and developed applications.\n - Acquired 1.5K MAU for a COVID-19 app.\n - Hosted an engineering community at our university with over 50 developers.",
   },
   {
     year: "2021",
     event: "CTO/Co-Founder\n\nat a stealth-mode startup",
     location_and_date: "San Francisco/U.S., Dec 2020 - Apr 2021",
     content:
-      "・Develop and execute all development systems such as infrastructure, database architecture, front/ back-end, and analytics.\n\n" +
-      "・Analyze data and user behaviors, and get 20 % more app-open counts per retaining user by implementing features.\n\n" +
-      "・Success with fund-raising $300K for pre-seed, and also with getting 100 supporters by crowd-funding.",
+      "Here Labs is a startup focused on enhancing communication within close relationships.\n\n - Developed and executed all development systems, including infrastructure, database architecture, front/back-end, and analytics.\n - Analyzed data and user behavior, increasing app-open counts per retaining user by 20% through feature implementation.\n - Successfully raised $300K in pre-seed funding and gained 100 supporters through crowdfunding.",
   },
   {
     year: "2022",
-    event: "PdM (Contractor)\n\nat Datafluct",
+    event: "PdM \n\nat Datafluct",
     atag: "https://datafluct.com",
     location_and_date: "Tokyo/Japan, July 2021 - April 2022",
     content:
-      "・Create specifications of multiple products in the AI agent project.\n\n" +
-      "・Operate development for apps and algorithms.",
+      "Engaged in the sustainable data business. We developed the personal AI agent based on an individual's history data including browser history, finance, etc.\n\n - Directed software/AI architectures, specifications,  and roadmaps.\n - Managed tasks for data scientists",
   },
   {
     year: "2023",
@@ -87,10 +67,7 @@ const careerList = [
     atag: "https://neurotechjp.com",
     location_and_date: "Remote, May 2021 - Present",
     content:
-      "・Direct the team and manage tasks.\n\n" +
-      "・Interview founders and researchers who stand the frontier in neurotech space.\n\n" +
-      "・Write articles(interviews, trends).\n\n" +
-      "・Research the market in the space and create original reports",
+      "NeurotechJP is dedicated to uncovering 'The Killer Applications of Neurotech' through in-depth interviews and articles with pioneers in cutting-edge neurotechnology research and development.\n\n - Directed the team and managed project tasks.\n - Conducted interviews with leading founders and researchers in the neurotech field.\n - Authored articles on interviews, industry trends, and insights.\n - Researched the neurotech market and produced original reports.",
   },
   {
     year: "2023",
@@ -98,16 +75,14 @@ const careerList = [
     atag: "https://neuronsinc.com",
     location_and_date: "Denmark, November 2021 - November 2023",
     content:
-      "Frontend engineer for the 'Predict' app.\n\n" +
-      "・Implement new features.\n\n" +
-      "・Write e2e/integration test code.",
+      "Neurons is the science-based SaaS company providing AI that predict the customers behavior for any creatives by using neuroscience and eye-tracking technology.\n\n Frontend engineer for the 'Predict' app.\n - Implemented new features.\n - Wrote e2e/integration test code.",
   },
   {
     year: "2023",
     event: "CTO at Demind Inc",
     atag: "https://www.linkedin.com/company/demind-inc/",
     location_and_date: "New York, September 2023 - Present",
-    content: "Making physiologically-aware personal AI.",
+    content: "Building a physiologically-aware personal AI.",
   },
 ]
 
