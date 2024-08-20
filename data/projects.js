0ef1299841aa3090b79ffcdb0c6d5e3fe8a13b49
wakkihaya <wakkihaya@gmail.com>
2024-08-20 17:03:59 +0900

    fix: text

--- data/projects.js
+++ data/projects.js
@@ -1,56 +1,50 @@
 const projectsList = [
   {
     title: '[OnGoing!]\n\n"NeurotechJP" (Media)',
-    content: "NeurotechJP is a neurotech interview media.",
+    content:
+      "A media website featuring blog articles, podcasts, and reports on neurotech",
     image: require("~/assets/images/recent-works/neurotech/neurotechJp.png"),
     atag: "https://neurotechjp.com",
   },
   {
     title: "BCI research project (University)",
     content:
-      "An individual research project of Emotional BCI in the univeristy. Clarified relationship between P300 and emotions.",
+      "An individual research project of Emotional BCI in the univeristy, exploring the relationship between P300 and emotions",
   },
   {
     title: '"Here" (Startup)',
     content:
-      "We(Here Labs,Inc) launched 'Here', a voice/video communication app for couples.",
+      "A mobile app, similar to Clubhouse, designed for couples in long-distance relationships",
     image: require("~/assets/images/recent-works/projects/Here.jpg"),
   },
   {
     title: '"Funbrella" (University)',
     content:
-      "A team project at Univ of Tsukuba. We developed `Funbrella`,the umbrella that projects a screen of your smartphone, and also launched the game using with 'Funbrella'",
+      "A mobile game where players navigate obstacles by tilting an umbrella, with the gameplay projected onto the umbrella's surface",
     image: require("~/assets/images/recent-works/projects/funbrella.png"),
   },
   {
     title: '"GatorTrader" (University)',
-    content:
-      "A team project at SFSU. We developed 'GatorTrader',C2C EC site for SFSU students.",
+    content: "E-commerce app for SFSU students",
     image: require("~/assets/images/recent-works/projects/gatortrader_formal.png"),
   },
   {
     title: '"paroli" (Startup)',
     content:
-      "We(Feedal,Inc) launched 'paroli', a remote-work communication tool with voice messages and real-time voice meetings.",
+      "A desktop app for seamless communication among remote work teams allowing voice messaging and real-time voice calls",
     image: require("~/assets/images/recent-works/projects/paroli.jpg"),
   },
   {
     title: '"vs.corona" (Startup)',
     content:
-      "We(Feedal,Inc) launched 'vs.corona', a curation website gathering COVID-19 news from reliable resouces such as governments.",
+      "A portal site that compiles COVID-related news, supported by local government in Japan",
     image: require("~/assets/images/recent-works/projects/vs-corona.png"),
   },
   {
     title: '"Feedal" (Startup)',
     content:
-      "We(Feedal,Inc) launched 'Feedal', a news-sharing platform for engineers.",
+      "A mobile platform like Twitter, but focused on news for engineers",
     image: require("~/assets/images/recent-works/projects/feedal.jpg"),
-  },
-  {
-    title: "Microsoft Business contest (Internship)",
-    content:
-      "Our team got 3rd place with a educative product 'Microsoft Canvas'.",
-    image: require("~/assets/images/recent-works/projects/MS_business_contest.png"),
   },
 ]
 
